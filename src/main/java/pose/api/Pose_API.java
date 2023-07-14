66a09fdd86bef32a2ef7c0a70b1882f6409e9fe3
Andre <andref@ua.pt>
2023-07-14 23:08:43 +0100

    managed to extract the pose

--- src/main/java/pose/api/Pose_API.java
+++ src/main/java/pose/api/Pose_API.java
@@ -1,7 +1,10 @@
 package pose.api;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +14,39 @@ public class Pose_API implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("pose-api");
+	private static MinecraftClient client;
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		
+		LOGGER.info("Pose API initialized!");
+		client = MinecraftClient.getInstance();
+
+		ClientTickEvents.START_CLIENT_TICK.register(client -> {
+			onUpdate();
+		});
+		}
+
+	public void onUpdate() {
+		if (client != null) {
+            ClientPlayerEntity player = client.player;
+            if (player != null) {
+                Vec3d pos = player.getPos();
+				float yaw = player.getYaw() % 360.0f;
+				float pitch = player.getPitch();
+
+				if (yaw < 0)
+                    yaw += 360.0f;
+
+                LOGGER.info("Player position: X=" + pos.x + " Y=" + pos.y + " Z=" + pos.z + " Yaw=" + yaw + " Pitch=" + pitch);
+            }
+        }
 	}
 
+	public float getYaw() {
+
+
+	}
 }
