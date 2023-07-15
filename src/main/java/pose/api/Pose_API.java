2f59416994031bc514c8371af51455f98adc0446
Andre <andref@ua.pt>
2023-07-15 12:41:29 +0100

    added the rest api

--- src/main/java/pose/api/Pose_API.java
+++ src/main/java/pose/api/Pose_API.java
@@ -9,13 +9,26 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Pose_API implements ModInitializer {
+// Imports for the REST API
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
+
+public class Pose_API implements ModInitializer, HttpHandler {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("pose-api");
 	private static MinecraftClient client;
+	double x;
+	double y;
+	double z;
 	float yaw;
+	float pitch;
+
+	private Undertow server;
 
 	@Override
 	public void onInitialize() {
@@ -28,6 +41,16 @@ public class Pose_API implements ModInitializer {
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			onUpdate();
 		});
+
+		// Create an Undertow server instance
+        server = Undertow.builder()
+                .addHttpListener(8080, "localhost")  // Set the desired port and hostname
+                .setHandler(getRoutingHandler())  // Set the routing handler as the request handler
+                .build();
+
+        // Start the server
+        server.start();
+
 		}
 
 	public void onUpdate() {
@@ -36,13 +59,46 @@ public class Pose_API implements ModInitializer {
             if (player != null) {
                 Vec3d pos = player.getPos();
 				yaw = player.getYaw() % 360.0f;
-				float pitch = player.getPitch();
+				pitch = player.getPitch();
 
 				if (yaw < 0)
                     yaw += 360.0f;
+
+				x = pos.x;
+				y = pos.y;
+				z = pos.z;
 
                 LOGGER.info("Player position: X=" + pos.x + " Y=" + pos.y + " Z=" + pos.z + " Yaw=" + yaw + " Pitch=" + pitch);
             }
         }
 	}
+
+	private RoutingHandler getRoutingHandler() {
+        // Create a routing handler to handle different API endpoints
+        RoutingHandler routingHandler = new RoutingHandler();
+
+        // Add a route for the /player endpoint
+        routingHandler.get("/player", exchange -> {
+            handlePlayerRequest(exchange);
+        });
+
+        return routingHandler;
+    }
+
+	private void handlePlayerRequest(HttpServerExchange exchange) {
+        // Retrieve the player's position and orientation
+        if (client != null) {
+                String playerData = "Player position: X=" + x + " Y=" + y + " Z=" + z +
+                        " Yaw=" + yaw + " Pitch=" + pitch;
+
+                // Set the response content type and send the player data as the response
+                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                exchange.getResponseSender().send(playerData);
+            }
+        }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        // This method is required by the HttpHandler interface but can be left empty
+    }
 }
